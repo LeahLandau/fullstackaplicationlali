370291a12833983c661d4e17002eb158800160de
LeaL <LeaL@skyvar.co.il>
2024-01-10 16:03:32 +0200

    sos

--- server/src/app.py
+++ server/src/app.py
@@ -1,21 +1,59 @@
-from flask import Flask
+# from flask import Flask
+# from flask_cors import CORS
+# from waitress import serve
+
+# # from routes.route import *
+# app = Flask(__name__, static_folder='../build', static_url_path='/')
+# # app = Flask(__name__)
+# CORS(app) 
+# # app.register_blueprint(routes)
+
+# # print('fdsjkghkljghdlgkjshgkdfgdgdsgdfgdsgdfgsdgsdg')
+# @app.route("/")
+# def index():  
+
+#     return app.send_static_file("index.html")
+
+# if __name__ == '__main__':
+#     serve(app, host="0.0.0.0", port=5000)
+import sys
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from waitress import serve
+import json
+# import numpy as np
 
-# from routes.route import *
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+sys.path.append('src') 
+# from module import *
+
 # app = Flask(__name__)
-CORS(app) 
-# app.register_blueprint(routes)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
-print('fdsjkghkljghdlgkjshgkdfgdgdsgdfgdsgdfgsdgsdg')
+
+CORS(app) 
+
+
 @app.route("/")
-def index():  
-    # return 'hhhhhhhhhhhhhhhhhhhhhh'
+def index():
+    # return "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" 
     return app.send_static_file("index.html")
+
+
+@app.route("/hello_world")
+def index1():
+    return app.send_static_file("index.html")
+
+@app.route('/blackening_pixels', methods=['POST'])
+def blackening_pixels():
+    body = request.get_json()
+    result = modify_image(body['imagePath'], np.array(body.get('polygonFrame')))
+    if type(result) != str:
+        return jsonify(result.args[0])
+    return jsonify(result)
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=5000)
+    
     
     
 # from flask import Flask
