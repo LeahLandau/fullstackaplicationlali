3c3c912da22d691edd9e4bdd0e26420e68062b8e
LeaL <LeaL@skyvar.co.il>
2024-01-16 17:08:10 +0200

    lali

--- server/src/app.py
+++ server/src/app.py
@@ -13,9 +13,9 @@ CORS(app)
 def index():
     return app.send_static_file("index.html")
 
-# @app.route('/api')
-# def index():
-#     return 'Image-Reduction'
+@app.route('/api')
+def index():
+    return 'Image-Reduction'
 
 # if __name__ == '__main__':
 #     serve(app, host="0.0.0.0", port=5000)
