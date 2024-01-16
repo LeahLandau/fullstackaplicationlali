3fd24a5b080780a3f09b4f194a8af8af16cf5144
LeaL <LeaL@skyvar.co.il>
2024-01-16 16:54:45 +0200

    lali

--- server/src/app.py
+++ server/src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from waitress import serve
 
-from routes.route import *
+# from routes.route import *
 
 
 app = Flask(__name__,static_folder='../../build', static_url_path='/')
@@ -13,9 +13,9 @@ app.register_blueprint(routes)
 def index():
     return app.send_static_file("index.html")
 
-@app.route('/api')
-def index():
-    return 'Image-Reduction'
+# @app.route('/api')
+# def index():
+#     return 'Image-Reduction'
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=5000)
