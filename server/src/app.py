b4bb13f513af5a127186edc7654c34d45f3f546a
LeaL <LeaL@skyvar.co.il>
2024-01-17 14:33:30 +0200

    lalli

--- server/src/app.py
+++ server/src/app.py
@@ -7,9 +7,13 @@ from routes.route import *
 app = Flask(__name__,static_folder='../../build', static_url_path='/')
 app.register_blueprint(routes)
 
+# @app.route("/")
+# def index():
+#     return app.send_static_file("index.html")
+
 @app.route("/")
 def index():
-    return app.send_static_file("index.html")
+    return app.send_static_file("images/grhy.jp2")
 
 @app.route('/api')
 def api():
