93440339f5ae5798df32f46ef4c4482af32df785
LeaL <LeaL@skyvar.co.il>
2024-01-18 11:39:20 +0200

    lalalalali

--- server/src/routes/route.py
+++ server/src/routes/route.py
@@ -1,40 +1,25 @@
-from flask import Blueprint, jsonify, request,abort
+from flask import Blueprint, jsonify, request, abort
 
 from modules.files_paths import *
 from modules.modify_image import *
+from modules.handle_error import *
 
 
 routes = Blueprint('routes', __name__)
 
-# @routes.route('/api/blackening_pixels', methods=['POST'])
-# def blackening_pixels():
-#     try:
-#         body = request.get_json()
-#         result = modify_image(body['imagePath'], body['polygonFrame'])
-#         print("2222222222")
-#         if isinstance(result, str):
-#             return jsonify(result), 200
-#         return jsonify(result.args[0]), 200
-#     except Exception as error:
-#         print("33333333")
-#         return handle_error(error)
 @routes.route('/api/blackening_pixels', methods=['POST'])
 def blackening_pixels():
-    # try:
-    body = request.get_json()
-    return jsonify(body)
-    #     result = modify_image(body['imagePath'], body['polygonFrame'])
-    #     return result, 200
-    # except FileNotFoundError as error:
-    #     error = handle_error(error)
-    #     abort(404, error)
-    # except Exception as error:
-    #     error = handle_error(error)
-    #     print("error",error)
-    #     abort(400, error)
-        
-    
-    
+    try:
+        body = request.get_json()
+        result = modify_image(body['imagePath'], body['polygonFrame'])
+        return result, 200
+    except FileNotFoundError as error:
+        error = handle_error(error)
+        abort(404, error)
+    except Exception as error:
+        error = handle_error(error)
+        abort(400, error)
+
 @routes.route('/api/get_images_names', methods=['GET'])
 def get_images_and_folders_names():
     try:
@@ -42,10 +27,21 @@ def get_images_and_folders_names():
         if(args.get('directory_path') is None):
             return jsonify('The directory path must be provided'), 400
         result = get_images_names(args.get('directory_path'))
-        print('result',result)
         if isinstance(result, FileNotFoundError):
             return result.args[0], 200
         return result, 200
     except Exception as error:
         return handle_error(error)
 
+@routes.route('/api/get_url_by_path', methods=['GET'])
+def get_url_by_path():
+    try:
+        args = request.args    
+        if(args.get('path') is None):
+            return jsonify('The path must be provided'),400
+        result = get_url_from_azure(args.get('path'))
+        if isinstance(result, str):
+            return jsonify(result),200
+        return jsonify(result.args[0]) ,200
+    except Exception as error:
+        return handle_error(error)
