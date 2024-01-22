da25fc0875ecfea9820e0c08272494e4d12416c3
LeaL <LeaL@skyvar.co.il>
2024-01-22 10:57:30 +0200

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import ImageReduction from './components/ImageReduction'
 
 function App() {
   return <>
-    <div>hdsnjhbhbhevbhregvbhdrjfbvhesdwjgsdhgskfgjmd</div>
+    <ImageReduction></ImageReduction>
   </>;
 }
 
