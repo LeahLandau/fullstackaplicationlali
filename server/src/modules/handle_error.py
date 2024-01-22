d436f825b9c881a18849162038768800b99a2d79
LeaL <LeaL@skyvar.co.il>
2024-01-22 09:23:48 +0200

    sos

---
+++ server/src/modules/handle_error.py
@@ -0,0 +1,16 @@
+import logging
+
+# def setup_logger():
+#     logger = logging.getLogger(__name__)
+#     logger.setLevel(logging.INFO)
+#     handler = logging.FileHandler(f"{__name__}.log", mode='w')
+#     formatter = logging.Formatter("%(asctime)s %(message)s")
+#     handler.setFormatter(formatter)
+#     logger.addHandler(handler)
+#     return logger
+
+# logger = setup_logger()
+
+def handle_error(error):
+    # logger.info(f'The error is: "{error}"')
+    return str(error)
