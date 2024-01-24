377b37d052387cb9f453cf42b3d988a66ae96972
LeaL <LeaL@skyvar.co.il>
2024-01-24 16:40:01 +0200

    with convert

--- server/src/app.py
+++ server/src/app.py
@@ -6,9 +6,9 @@ from waitress import serve
 app = Flask(__name__,static_folder='/static', static_url_path='/')
 app.register_blueprint(routes)
 
-@app.route("/")
-def index():
-    return app.send_static_file("index.html")
+# @app.route("/")
+# def index():
+#     return app.send_static_file("index.html")
 
 @app.route('/api')
 def api():
