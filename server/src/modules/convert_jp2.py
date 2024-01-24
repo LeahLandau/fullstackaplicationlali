70fe923717d4838e1e5d0ca806c08085d7dce7ce
LeaL <LeaL@skyvar.co.il>
2024-01-24 16:20:28 +0200

    with convert

--- server/src/modules/convert_jp2.py
+++ server/src/modules/convert_jp2.py
@@ -12,7 +12,6 @@ def read_image_pixels(file_path):
         image_pixels = src.read()
     return image_pixels
 
-
 def save_image_pixels(pixels, file_path):
     pixels_reshaped = pixels.squeeze()
     pixels_normalized = (pixels_reshaped - pixels_reshaped.min()) / (pixels_reshaped.max() - pixels_reshaped.min()) * 255
